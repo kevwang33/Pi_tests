bb48979f48fbc748040662959ed815c31bedc25a
Kevin Wang <128927473+kevwang33@users.noreply.github.com>
2026-03-14 20:31:18 -0400

    enhanced servo pin

--- servo_control.py
+++ servo_control.py
@@ -1,9 +1,12 @@
-from gpiozero import AngularServo
+from gpiozero import Device, AngularServo
+from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
+
+Device.pin_factory = PiGPIOFactory()
 
 servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
-while (True):
+while True:
     servo.angle = 90
     sleep(2)
     servo.angle = 0
